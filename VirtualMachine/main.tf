cecf52e9e0651b65bc3c14ac7a6044afc1942c08
Shivendra Gautam <68961087+gautam-shiv@users.noreply.github.com>
2020-10-14 13:00:13 +0530

    Update main.tf
    Added Boot diagnostics block under virtual machine block

--- VirtualMachine/main.tf
+++ VirtualMachine/main.tf
@@ -4,8 +4,9 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "myrg" {
-  name = "dd-test-sample-rg-pasv"
+  name     = "dd-test-sample-rg-pasv"
   location = "centralindia"
+  
   tags = {
         environment = "Terraform Demo"
     }
@@ -59,9 +60,9 @@ resource "azurerm_network_security_group" "mytfnsg" {
 
 # create a virtual network
 resource "azurerm_virtual_network" "mytfvnet" {
-    name = "${toLower(var.projectname)}-${toLower(var.zone)}-${var.environmentName}-vnet"
-    address_space = ["10.0.0.0/16"]
-    location = azurerm_resource_group.myrg.location
+    name                = "${toLower(var.projectname)}-${toLower(var.zone)}-${var.environmentName}-vnet"
+    address_space       = ["10.0.0.0/16"]
+    location            = azurerm_resource_group.myrg.location
     resource_group_name = azurerm_resource_group.myrg.name
 
     depends_on = [azurerm_network_security_group.mynsg.id]
@@ -100,36 +101,37 @@ resource "azurerm_virtual_machine" "myvm" {
  #for_each = { for each_vm in "${var.vm_details_mapping}" : each_vm["name"] => each_vm }
   location              = azurerm_resource_group.myrg.location
   name                  = "${toLower(var.projectname)}-${toLower(var.zone)}-${var.environmentName}-vm"
-  network_interface_ids = ["${element(azurerm_network_interface.azurermnetworkinterface.*.id,each.value.nic_index )}"]
+  network_interface_id  = azurerm_network_interface.mytfnic.id
   resource_group_name   = azurerm_resource_group.myrg.name
   vm_size               = "${var.vm_size}"
 
   storage_image_reference {
-        publisher = "MicrosoftWindowsServer"
-        offer = "WindowsServer"
-        sku = ${var.os_version}
-        version = "latest"
+        publisher       = "MicrosoftWindowsServer"
+        offer           = "WindowsServer"
+        sku             = ${var.os_version}
+        version         = "latest"
     }
 
     storage_os_disk {
-        name = "myosdisk"
-        vhd_uri = "${azurerm_storage_account.helloterraformstorage.primary_blob_endpoint}${azurerm_storage_container.helloterraformstoragestoragecontainer.name}/myosdisk.vhd"
-        caching = "ReadWrite"
-        create_option = "FromImage"
+        name              = "myosdisk"
+        caching           = "ReadWrite"
+        create_option     = "FromImage"
+        managed_disk_type = ${var.os_disk_type}
     }
 
     os_profile {
-        computer_name =   ${var.vmname}
+        computer_name  =   ${var.vmname}
         admin_username =  ${var.admin_name}
         admin_password =  ${var.admin_password}
     }
 
-    os_profile_linux_config {
-        disable_password_authentication = false
+    boot_diagnostics {
+        storage_account_uri = azurerm_storage_account.mytfstorage.id.primaryEndpoints.blob
     }
 
     tags {
         environment = "Terraform Demo"
     }
-}
 
+    depends_on      = [azurerm_storage_account.mytfstorage.id, azurerm_network_interface.mytfnic.id]
+}
