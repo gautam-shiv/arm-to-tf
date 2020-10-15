dc7df96c8ff4c2f4994f9faecb6b3daf026cda35
Vinayak Rastogi <vinayakrrrrr@gmail.com>
2020-10-15 14:28:18 +0530

    Update main.tf

--- Eventhub Cluster/main.tf
+++ Eventhub Cluster/main.tf
@@ -12,15 +12,7 @@ resource "azurerm_resource_group" "myrg" {
 }
 
 resource "azurerm_eventhub" "example" {
-  name                = "acceptanceTestEventHub"
-  namespace_name      = azurerm_eventhub_namespace.example.name
-  resource_group_name = azurerm_resource_group.example.name
-  partition_count     = 2
-  message_retention   = 1
-}
-
-resource "azurerm_eventhub" "example" {
-  name                = "${toLower(var.projectName)}-{toLower(var.zone)}-{toLower(var.environmentName)}-ehc"
+  name                = "${toLower(var.projectName)}-${toLower(var.zone)}-${toLower(var.environmentName)}-ehc"
   location            = azurerm_resource_group.myrg.location
   resource_group_name = azurerm_resource_group.myrg.name
   sku                 = "Dedicated"
