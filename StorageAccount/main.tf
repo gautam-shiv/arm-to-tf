a84105f20d221e10f81f937656e66dec3a1a6de8
Shivendra Gautam <68961087+gautam-shiv@users.noreply.github.com>
2020-10-15 17:13:57 +0530

    Update main.tf
    Update access policies of Key Vault

--- StorageAccount/main.tf
+++ StorageAccount/main.tf
@@ -46,7 +46,23 @@ resource "azurerm_key_vault" "mytfkv" {
   resource_group_name = azurerm_resource_group.myrg.name
   tenant_id           = data.azurerm_client_config.current.tenant_id
   sku_name            = "standard"
+  
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
 
+    key_permissions = [
+      "create",
+      "get",
+    ]
+
+    secret_permissions = [
+      "set",
+      "get",
+      "delete",
+    ]
+  }
+  
   tags = {
     environment = "Terraform Demo"
   }
